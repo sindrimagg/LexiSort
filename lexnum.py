7b77cb2e01a1e7c80cdd5df14e0e990a5a3e2c78
Sindri Magnússon <sim42@hi.is>
2025-04-28 11:19:41 +0200

    Made a little refactoring of the lexnum.py file

--- lexnum.py
+++ lexnum.py
@@ -1,60 +1,33 @@
-import re,os
+import re,os,itertools
 
-def combine(txt, nums, max_len):
-    res = ''
-    t = 0
-    n = 0
-    start = 2
-
-    if len(txt) > len(nums):
-        start = 0
-    elif len(txt) < len(nums):
-        start = 1
-    else:
-        if not txt:
-            assert(len(nums) == 0)
-            return nums
-        else:
-            assert(nums[0] != txt[0])
-            if nums[0]:
-                start = 0
-            elif txt[0]:
-                start = 1
-
-    assert(start != 2)
-
-    last = t + n
-    while t < len(txt) or n < len(nums):
-        if start == 0:
-            res += txt[t]
-            t += 1
-            start = 1
-        else:
-            res += '0'*(max_len[n] - len(nums[n])) + nums[n]
-            n += 1
-            start = 0
-    return res
+def weave(*iterable):
+    for element in itertools.chain.from_iterable(itertools.zip_longest(*iterable)):
+        if element is not None:
+            yield element
+    
+def combine(txt, nums, max_lens):
+    padded_nums = ('0'*(max_len - len(num)) + num for num, max_len in zip(nums, max_lens))
+    return ''.join(weave(txt, padded_nums))
 
 filenames = os.listdir()
+digs = re.compile(r'\d+')
+sep = re.compile(fr'{os.sep}')
 
 hm = {}
 for file in filenames:
-    text = re.split(r'\d+', file)
-    nums = re.split(r'[^\d]+', file)
+    text = re.split(digs, file)
+    nums = re.findall(digs, file)
 
-    tup_text = tuple(text)
+    tup_text = f'{os.sep}'.join(text)
     if tup_text in hm:
         hm[tup_text].append(nums)
     else:
         hm[tup_text] = [nums]
 
-for text in hm:
-    size = len(hm[text][0])
-    max_lengths = [0 for _ in range(size)]
-    for nums in hm[text]:
-        for (ix,num) in enumerate(nums):
-            max_lengths[ix] = max(max_lengths[ix], len(num))
-    for nums in hm[text]:
-        text = list(text)
-        out = combine(text, nums, max_lengths)
-        print('Was: ' + combine(text, nums, [0 for _ in nums]) + '\nBecomes: ' + out + '\n')
+for text,num_list in hm.items():
+    max_lengths = [max(map(lambda x: len(x), col)) for col in zip(*num_list)]
+
+    txt = re.split(sep, text)
+    for num in num_list:
+        out = combine(txt, num, max_lengths)
+        print('Was: ' + ''.join(weave(txt, num)) + '\nBecomes: ' + out + '\n')
